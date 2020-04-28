b0c6868e0d60d8b9bbeb81617306b37e7fda3e83
Adarsh <techwizardadarsh@gmail.com>
2020-04-28 10:31:14 +0530

    Updates

---
+++ precache-manifest.880a64748b71f6970dc59f979136c163.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "170137e0cd40c57ae6fb6db687004cfb",
+    "url": "/React-Youtube-App/index.html"
+  },
+  {
+    "revision": "25c9029f9434bca3f11c",
+    "url": "/React-Youtube-App/static/css/main.23c2e205.chunk.css"
+  },
+  {
+    "revision": "f5499580ab179d3be0eb",
+    "url": "/React-Youtube-App/static/js/2.d741e0e2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Youtube-App/static/js/2.d741e0e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25c9029f9434bca3f11c",
+    "url": "/React-Youtube-App/static/js/main.2a0fb7cd.chunk.js"
+  },
+  {
+    "revision": "fe623cac911c55827f5e",
+    "url": "/React-Youtube-App/static/js/runtime-main.62760b15.js"
+  }
+]);
